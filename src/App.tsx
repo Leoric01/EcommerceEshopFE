8bea35fa8d0bb13a32b09a0160a3184d8b3ae677
Leoric01 <troglodit.nick@seznam.cz>
2024-10-20 19:29:42 +0200

    Normalize line endings to CRLF

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import './App.css';
-import {Button} from "@mui/material";
-
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">HASHUBFHASBFHAS</h1>
-        <Button variant="contained">TEST</Button>
-    </div>
-  );
-}
-
-export default App;
+﻿import React from 'react';import './App.css';import {Button} from "@mui/material";function App() {  return (    <div className="App">      <h1 className="text-3xl font-bold underline">HASHUBFHASBFHAS</h1>        <Button variant="contained">TEST</Button>    </div>  );}export default App;
