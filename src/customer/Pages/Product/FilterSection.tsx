43087fc979cf044ed81ffa1635e96644d3ab6bb8
Leoric01 <troglodit.nick@seznam.cz>
2024-10-21 06:59:00 +0200

    filter by color is getting nice progress

--- src/customer/Pages/Product/FilterSection.tsx
+++ src/customer/Pages/Product/FilterSection.tsx
@@ -1,9 +1,61 @@
+import {
+  Button,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { teal } from "@mui/material/colors";
 import React from "react";
+import { Colors } from "../../../Data/filter/Color";
 
 const FilterSection = () => {
   return (
-    <div>
-      <h1>f</h1>
+    <div className="-z-50 space-y-5 bg-white">
+      <div className="flex items-center justify-between h-[40px] px-9 lg:border-r">
+        <p className="text-lg font-semibold">Filters</p>
+        <Button size="small" className="text-teal-600 cursor-pointer font-bold">
+          clear all
+        </Button>
+      </div>
+      <Divider />
+      <section>
+        <FormControl>
+          <FormLabel
+            sx={{
+              fontSize: "16px",
+              fontWeight: "bold",
+              color: teal[500],
+              pb: "14px",
+            }}
+            className="text-2xl font-semibold"
+            id="color"
+          >
+            Color
+          </FormLabel>
+          <RadioGroup aria-labelledby="color" defaultValue="" name="color">
+            {Colors.map((item) => (
+              <FormControlLabel
+                value={item.hex}
+                control={<Radio />}
+                label={
+                  <div className="flex items-center gap-2">
+                    <p>{item.name}</p>
+                    <span
+                      style={{ backgroundColor: item.hex }}
+                      className={`h-5 w-5 rounded-full inline-block ${
+                        item.name === "White" ? "border border-gray-400" : ""
+                      }`}
+                    ></span>
+                  </div>
+                }
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+      </section>
     </div>
   );
 };
