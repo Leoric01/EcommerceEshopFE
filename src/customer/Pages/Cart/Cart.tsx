a8fe0972a4f641e7ff15e46cb4d78457f607f54d
Leoric01 <troglodit.nick@seznam.cz>
2024-10-21 19:17:59 +0200

    continue with cart page

--- src/customer/Pages/Cart/Cart.tsx
+++ src/customer/Pages/Cart/Cart.tsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import CartItem from './CartItem'
+import React, { useState } from "react";
+import CartItem from "./CartItem";
+import { LocalOffer } from "@mui/icons-material";
+import { teal } from "@mui/material/colors";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 const Cart = () => {
+  const [couponCode, setCouponCode] = useState("");
+  const handleChange = (e: any) => {
+    setCouponCode(e.target.value);
+  };
+  
+  const handleApplyCoupon = () => {
+    return null;
+  }
+
   return (
-    <div className='pt-10 px-5 sm:px-10 md:px-40 min-h-screen'>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            <div className="cartItemSection lg:col-span-2 space-y-2">
-                {[1,1,1,1,1,1].map((item)=> <CartItem/>)}
-            </div>
-            <div className="col-span-1 text-sm space-y-3 border">
-                <div className="">
-
-                </div>
-            </div>
+    <div className="pt-10 px-5 sm:px-10 md:px-40 min-h-screen">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <div className="cartItemSection lg:col-span-2 space-y-2">
+          {[1, 1, 1, 1, 1, 1].map((item) => (
+            <CartItem />
+          ))}
         </div>
+        <div className="col-span-1 text-sm space-y-3">
+          <div className="border rounded-md px-5 py-3 space-y-4">
+            <div className="flex gap-3 text-sm items-center">
+              <div className="flex gap-3 text-sm items-center">
+                <LocalOffer sx={{ color: teal[600], fontSize: "17px" }} />
+              </div>
+              <span>Apply Coupons</span>
+            </div>
+            <div className="flex align-middle justify-between">
+              <TextField
+                onChange={handleChange}
+                id="outlined-basic"
+                label="Coupon code"
+                size="small"
+                variant="outlined"
+              />
+              <Button size="small" variant="contained" onClick={handleApplyCoupon}>
+                Apply
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- src/customer/Pages/Cart/CartItem.tsx
+++ src/customer/Pages/Cart/CartItem.tsx
@@ -1,8 +1,9 @@
-import { Remove } from "@mui/icons-material";
+import { Close, Remove } from "@mui/icons-material";
 import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
+import Add from "@mui/icons-material/Add";
 import Divider from "@mui/material/Divider";
 import React, { useState } from "react";
+import { IconButton } from "@mui/material";
 
 const CartItem = () => {
   const [quantity, setQuantity] = useState(1);
@@ -25,24 +26,36 @@ const CartItem = () => {
           </p>
           <p className="text-sm">7 days replacement available</p>
           <p className="text-sm text-gray-500">
-            <strong>quantity : </strong> {quantity}
+            <strong>quantity : </strong> {quantity}{" "}
+            <Divider orientation="vertical" variant="middle" flexItem />
+            <strong>Price per piece : 900</strong>
           </p>
         </div>
       </div>
       <Divider />
-      <div className="px-5 py-2 flex justify-between items-center">
-        <div className="flex items-center gap-2 w-[140px] justify-between">
-          <Button
-            disabled={quantity <= 0}
-            onClick={() => setQuantity(quantity - 1)}
-          >
-            <Remove />
-          </Button>
-          <span>{quantity}</span>
-          <Button onClick={() => setQuantity(quantity + 1)}>
-            <AddIcon />
-          </Button>
+      <div className="flex justify-between items-center">
+        <div className="px-5 py-2 flex justify-between items-center">
+          <div className="flex items-center gap-2 w-[140px] justify-between">
+            <Button
+              disabled={quantity <= 0}
+              onClick={() => setQuantity(quantity - 1)}
+            >
+              <Remove />
+            </Button>
+            <span>{quantity}</span>
+            <Button onClick={() => setQuantity(quantity + 1)}>
+              <Add />
+            </Button>
+          </div>
         </div>
+        <div className="pr-5">
+          <p className="text-gray-700 font-medium">900 €</p>
+        </div>
+      </div>
+      <div className="absolute top-1 right-1">
+        <IconButton>
+          <Close />
+        </IconButton>
       </div>
     </div>
   );
