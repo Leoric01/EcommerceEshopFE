94563663e7d6a8c56994305c8613d41cd0bda909
Leoric01 <troglodit.nick@seznam.cz>
2024-10-24 12:17:24 +0200

    generated endpoints/models

---
+++ src/Api/apis/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddressControllerApi';
+export * from './AdminControllerApi';
+export * from './AdminCouponControllerApi';
+export * from './AuthControllerApi';
+export * from './CartControllerApi';
+export * from './DealControllerApi';
+export * from './HomeCategoryControllerApi';
+export * from './OrderControllerApi';
+export * from './ProductControllerApi';
+export * from './ReviewControllerApi';
+export * from './SellerControllerApi';
+export * from './SellerOrderControllerApi';
+export * from './SellerProductControllerApi';
+export * from './TransactionControllerApi';
+export * from './UserControllerApi';
+export * from './WishlistControllerApi';
