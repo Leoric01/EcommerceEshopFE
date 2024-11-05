b56c06cfa14c2913e6f4298f52b84b9f4062f9e8
Leoric01 <troglodit.nick@seznam.cz>
2024-11-05 08:26:20 +0100

    next three dasboards to controll home page works nice

--- src/Admin/Pages/HomePage/ElectronicTable.tsx
+++ src/Admin/Pages/HomePage/ElectronicTable.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import HomeCategoryTable from "./HomeCategoryTable";
+import { Home as HomeInterface } from "../../../Api/models";
+import { customerApi } from "../../../State/configAxios/customerApi";
+import { homeCategories } from "../../../Data/HomeCategories";
 
 const ElectronicTable = () => {
+  const [homePage, setHomePage] = useState<HomeInterface>({});
+
+  const handleCreateHomePage = async () => {
+    try {
+      const response = await customerApi.createHomeCategories(homeCategories);
+      setHomePage(response.data);
+      console.log("Response data: ", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleCreateHomePage();
+  }, []);
+
   return (
     <div>
-      <HomeCategoryTable categories={[]} />
+      <HomeCategoryTable categories={homePage.electricCategories || []} />
     </div>
   );
 };
--- src/Admin/Pages/HomePage/GridTable.tsx
+++ src/Admin/Pages/HomePage/GridTable.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import HomeCategoryTable from "./HomeCategoryTable";
+import { Home as HomeInterface } from "../../../Api/models";
+import { customerApi } from "../../../State/configAxios/customerApi";
+import { homeCategories } from "../../../Data/HomeCategories";
 
 const GridTable = () => {
+  const [homePage, setHomePage] = useState<HomeInterface>({});
+  const handleCreateHomePage = async () => {
+    try {
+      const response = await customerApi.createHomeCategories(homeCategories);
+      setHomePage(response.data);
+      console.log("Response data: ", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    handleCreateHomePage();
+  }, []);
   return (
     <div>
-      <HomeCategoryTable categories={[]} />
+      <HomeCategoryTable categories={homePage.grid || []} />
     </div>
   );
 };
